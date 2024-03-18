680e1f9a225fe3318a4d744854aa447d8ace62cb
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2024-03-18 18:22:46 -0500

    Added example classes

---
+++ 24.1Taller3DLunes/Week1/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3D
+{
+    internal class Rectangle
+    {
+        private float b;
+        private float h;
+
+        public Rectangle(float b, float h)
+        {
+            this.b = b;
+            this.h = h;
+        }
+
+        public float GetArea()
+        {
+            return b * h;
+        }
+
+        public float GetPerimeter()
+        {
+            return 2 * (b + h);
+        }
+    }
+}
